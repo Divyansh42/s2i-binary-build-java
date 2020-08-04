c9f6642298728a9f9edd384a81d224870b509228
Divyansh42 <diagrawa@redhat.com>
2020-08-04 13:05:01 +0530

    pipeline modified

--- script.sh
+++ script.sh
@@ -2,6 +2,7 @@ oc delete -f .
 oc create -f https://raw.githubusercontent.com/tektoncd/catalog/master/task/git-clone/0.1/git-clone.yaml
 oc create -f https://raw.githubusercontent.com/tektoncd/catalog/master/task/maven/0.1/maven.yaml
 oc create -f https://raw.githubusercontent.com/tektoncd/catalog/master/task/maven/0.1/tests/resources.yaml
+kubectl apply -f https://raw.githubusercontent.com/tektoncd/catalog/master/task/skopeo-copy/0.1/skopeo-copy.yaml
 oc create -f clusterrolebindings.yaml
 oc create -f s2i-binary.yaml
 oc create -f pipeline.yaml
